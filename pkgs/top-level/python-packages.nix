120c9cce17d7621dd96fdee5f8a15dbfd93b71eb
Vika <vika@fireburn.ru>
2024-04-06 22:16:34 +0300

    Revert "openvino: 2023.3.0 -> 2024.0.0"
    This reverts commit 4b8720f332de98d17b9ceedccbf337b4bce15a1d.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9753,7 +9753,7 @@ self: super: with self; {
 
   openvino = callPackage ../development/python-modules/openvino {
     openvino-native = pkgs.openvino.override {
-      python3Packages = self;
+      inherit python;
     };
   };
 
