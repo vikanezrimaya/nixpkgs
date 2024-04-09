a05ee8c7055c97b31318d45539fa8de679ef1a33
Vika <vika@fireburn.ru>
2024-04-10 01:36:32 +0300

    sillytavern: fix user directory being non-writable

--- pkgs/by-name/si/sillytavern/package.nix
+++ pkgs/by-name/si/sillytavern/package.nix
@@ -27,7 +27,7 @@ trap fusecleanup 1 2 3 6 15
   mkdir -p "$_config_dir/opt" "$_config_dir/optdiff"
   fusecleanup
   echo "Fuse mounting of $_config_dir/optdiff and $_SHAREPREFIX to $_config_dir/opt"
-  ${pkgs.unionfs-fuse}/bin/unionfs -o cow -o umask=000 $_SHAREPREFIX=ro:$_config_dir/optdiff=rw $_config_dir/opt|| exit 1
+  ${pkgs.unionfs-fuse}/bin/unionfs -o cow -o umask=000 $_config_dir/optdiff=rw:$_SHAREPREFIX=ro $_config_dir/opt|| exit 1
 
   printf $_yellow_color_code "If automatic unmounting fails, run this command:" "fusermount -zu $_config_dir/opt" >&2
 
@@ -41,8 +41,10 @@ trap fusecleanup 1 2 3 6 15
     mkdir -p $_config_dir/optdiff/default/
   fi
 
-  cp -R $_SHAREPREFIX/publicc/* $_config_dir/optdiff/public
-  cp -R $_SHAREPREFIX/defaultt/* $_config_dir/optdiff/default
+  #cp -R $_SHAREPREFIX/publicc/* $_config_dir/optdiff/publicc
+  #rm $_config_dir/optdiff/publicc/settings.json
+  #cp -R $_config_dir/optdiff/publicc/* $_config_dir/optdiff/public/
+  #cp -R $_SHAREPREFIX/defaultt/* $_config_dir/optdiff/default
 
   chmod +rw -R $_config_dir/optdiff
 
@@ -102,8 +104,8 @@ buildNpmPackage rec {
         mv * $out/share/${pname}
 
     # Name here and at configuration folder can't be equal otherwise it will conflict and make the config folder unwritable (for the files/folder with the same name)
-        mv $out/share/${pname}/public $out/share/${pname}/publicc
-        mv $out/share/${pname}/default $out/share/${pname}/defaultt
+        #mv $out/share/${pname}/public $out/share/${pname}/publicc
+        #mv $out/share/${pname}/default $out/share/${pname}/defaultt
 
         runHook postInstall
   '';
@@ -117,7 +119,7 @@ buildNpmPackage rec {
     downloadPage = "https://github.com/SillyTavern/SillyTavern/releases";
     homepage = "https://docs.sillytavern.app/";
     mainProgram = "sillytavern";
-    license = licenses.agpl3;
+    license = licenses.agpl3Only;
     maintainers = [ maintainers.aikooo7 ];
   };
 }
